b65c457836e48ffb91888e0f7dd3bea8ed16de3e
Tom <tom_hejiaxi@163.com>
2023-11-11 07:55:16 +0800

    docker

--- build/docker/build-push-image.sh
+++ build/docker/build-push-image.sh
@@ -6,6 +6,6 @@ tag=$1
 echo $pwd
 echo $dir
 echo $tag
-docker run --rm -v ${root}:/app -v ${pwd}:/out golang:1.21-alpine3.18 go build -o /out/cnet /app/cmd/cnet/main.go
+docker run --rm -w /app -v ${root}:/app -v ${pwd}:/out golang:1.21-alpine3.18 go build -o /out/cnet ./cmd/cnet/main.go
 ls -anl ${pwd}/cnet
 docker buildx build --platform linux/amd64,linux/arm64 ${pwd} -t $tag --push
